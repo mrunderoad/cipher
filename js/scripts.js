a35928283b8f8268c650afc0c6605d5587b765d1
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-18 15:02:38 -0700

    Add more jQuery and JS features
    co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,30 @@
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("Stop clicking me!!");
+  });
+});
+
+
 const sentence = prompt("Enter your sentence here:");
 alert("Your sentence is: " + sentence + ".");
+
+function firstLast (inputSentence) {
+  const firstLetter = inputSentence[0].toUpperCase();
+  const sentenceLength = inputSentence.length;
+  const lastLetter = inputSentence[sentenceLength - 1].toUpperCase();
+  return firstLetter + lastLetter;
+}
+const firstLastLetters = firstLast(sentence);
+
+function reverse (inputWords) {
+  return inputWords[1] + inputWords[0];
+}
+
+const reversedLetters = reverse(firstLastLetters);
+
+function newSentence (inputSentence, letters) {
+  return inputSentence + letters;
+}
+
+alert(newSentence(sentence, reversedLetters));
+
